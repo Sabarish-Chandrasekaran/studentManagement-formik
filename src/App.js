ee8f4a880c2c3ea948b38b810c82cb0ef99d21f6
Sabarish Chandrasekaran <sabarish27k6@gmail.com>
2022-07-29 19:09:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
+import AllStudents from "./components/AllStudents";
+import AddStudents from "./components/AddStudents";
+import EditStudent from "./components/EditStudent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+
+export const StudentContext = React.createContext();
 
 function App() {
+  let data = {
+    earning: "45,000",
+    annual: "3,50,000",
+    task: 50,
+    pending: 26,
+  };
+
+  let [students, setStudents] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div style={{ display: "grid", gridTemplateColumns: "17% 80%" }}>
+          <div>
+            <Sidebar />
+          </div>
+          <div>
+            <StudentContext.Provider value={{ students, setStudents }}>
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard data={data} />} />
+                <Route path="/all-students" element={<AllStudents />} />
+                <Route path="/add-student" element={<AddStudents />} />
+                <Route path="/edit-student/:id" element={<EditStudent />} />
+                <Route path="/" element={<Dashboard data={data} />} />
+              </Routes>
+            </StudentContext.Provider>
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
